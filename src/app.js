2e5bea76e5f29411f9c2e1fdf9384352643d34ba
Vaibhav <your-email@example.com>
2025-05-23 17:02:56 +0530

    Add cookie-parser and jsonwebtoken for authentication; implement login and profile retrieval endpoints

--- src/app.js
+++ src/app.js
@@ -3,13 +3,15 @@ const { adminAuth } = require('./middleware/adminAuth');
 const connectDb = require("./config/database")
 const bcrypt = require("bcrypt")
 const User = require("./models/user.js")
-const { validateSingUpData } = require("./utils/validation.js")
+const { validateSingUpData } = require("./utils/validation.js");
+const cookieParser = require('cookie-parser');
+const jwt = require("jsonwebtoken")
+
 const app = express();
 
 app.use(express.json());
+app.use(cookieParser())
 app.post("/singup", async (req, res) => {
-
-
     try {
         validateSingUpData(req);
         const { firstName, lastName, emailId, password } = req.body;
@@ -29,6 +31,51 @@ app.post("/singup", async (req, res) => {
         res.status(400).send("user not created successfully " + error.message)
 
     }
+})
+
+app.post("/login", async (req, res) => {
+
+
+    try {
+        const { emailId, password } = req.body;
+        const user = await User.findOne({ emailId: emailId })
+        if (!user) {
+            throw new Error("User is not present in DB")
+        }
+        const isPasswordValid = await bcrypt.compare(password, user.password);
+        if (isPasswordValid) {
+            const token = await jwt.sign({_id : user._id}, "hhshshhshshsh")
+            res.cookie("token",token)
+            res.status(200).send("Login Successfull!!!");
+        } else {
+            res.status(400).send("password not Match!!!")
+        }
+    } catch (error) {
+        res.status(400).send("problem while login " + error.message)
+    }
+})
+
+app.get("/profile", async(req, res)=>{
+    try {
+        const cookies = req.cookies;
+        const {token} = cookies;
+        if(!token){
+            throw new Error("Token not fount")
+        }
+        //validate my token
+        const decodedMessage = await jwt.verify(token, "hhshshhshshsh");
+        const user = await User.findById(decodedMessage._id)
+        if(!user){
+            throw new Error("user not found")
+        }
+        console.log(user);
+    
+        res.send(user)
+    } catch (error) {
+        res.status(400).send("error while fectcing profile "+res.message)
+    }
+    
+    
 })
 
 app.get("/user", async (req, res) => {
@@ -145,7 +192,8 @@ connectDb().then(() => {
     console.log("database connection stable");
     app.listen(3000);
     console.log('Server is running on port 3000');
-    }) .catch(() => {console.log("database connection disable");
-    }
-    )
+}).catch(() => {
+    console.log("database connection disable");
+}
+)
 
