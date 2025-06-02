6dc397b678626a316b2af8828746e5c9ec6dc6e6
Vaibhav <your-email@example.com>
2025-06-02 11:27:16 +0530

    Add feed endpoint to retrieve users excluding hidden connections

--- src/routes/user.js
+++ src/routes/user.js
@@ -1,7 +1,10 @@
 const express = require("express");
 const { userAuth } = require("../middleware/auth");
 const ConnectionRequest = require("../models/connectionRequest");
+const User = require("../models/user");
 const userRouter =express.Router();
+
+const SAFE_USER_DATA = "firstName lastName gender photoUrl age about skills"
 
 //all panding status request
 userRouter.get("/user/requests", userAuth, async(req,res)=>{
@@ -23,7 +26,6 @@ userRouter.get("/user/requests", userAuth, async(req,res)=>{
 })
 
 userRouter.get("/user/connections", userAuth, async(req,res)=>{
-const SAFE_USER_DATA = "firstName lastName gender photoUrl age about skills"
     try {
         const loggedInUser = req.user;        
         const connection = await ConnectionRequest.find({ 
@@ -46,5 +48,40 @@ const SAFE_USER_DATA = "firstName lastName gender photoUrl age about skills"
     } catch (error) {
         return res.status(400).send("ERR: "+ error.message)
     }
+})
+
+userRouter.get("/feed", userAuth, async(req,res)=>{
+   try {
+     const loggedInUser = req.user;
+     const connectionRequest = await ConnectionRequest.find({
+         $or:[{
+             fromUserId : loggedInUser._id
+         },{
+             toUserId : loggedInUser._id 
+         }]
+
+     }).select("toUserId fromUserId") 
+
+     const hideUsersFromFeed = new Set();
+      connectionRequest.forEach(req => {
+        hideUsersFromFeed.add(req.fromUserId.toString());
+        hideUsersFromFeed.add(req.toUserId.toString());
+     })
+     const users = await User.find({
+         $and : [
+             {
+                 _id : { $nin : Array.from(hideUsersFromFeed)}
+                },{
+                    _id : { $ne : loggedInUser._id}
+                }
+            ]
+        }).select(SAFE_USER_DATA)
+        console.log(users);
+
+res.send(users)
+
+   } catch (error) {
+    res.status(400).send("Err: "+ error.message)
+   }
 })
 module.exports = userRouter
