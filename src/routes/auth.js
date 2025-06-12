56ea1e709dce6268e325e944bd7084e2ad70230e
Vaibhav <your-email@example.com>
2025-06-12 17:48:17 +0530

    Refactor signup route to improve validation and response handling

--- src/routes/auth.js
+++ src/routes/auth.js
@@ -9,9 +9,24 @@ authRouter.post("/singup", async (req, res) => {
     try {
         validateSingUpData(req);
         const { firstName, lastName, emailId, password , skills, gender} = req.body;
+        
+        
+        if(firstName.length > 20){
+            throw new Error("Firstname should be smaller then 20 char");
+        }
+        if(lastName.length > 20){
+            throw new Error("Lastname should be smaller then 20 char");
+        }
+        if(await User.findOne({ emailId })){
+             throw new Error("Email is already present");
+        }
+      
+
+        
         const hashedPassword = await bcrypt.hash(password, 10);
 
-        const user = new User({
+
+        const user =await new User({
             firstName,
             lastName,
             emailId,
@@ -20,11 +35,19 @@ authRouter.post("/singup", async (req, res) => {
             gender
         })
 
-        await user.save()
-        res.status(200).send("user created successfully")
+       const savedUser = await user.save()
+       const token =  await user.getJWT()
+        
+            res.cookie("token",token,{
+                expires : new Date(Date.now() + 8 * 3600000)
+            })
+    res
+      .status(200)
+      .json({ message: "User added successfully", data: savedUser });
+        
 
-    } catch (error) {
-        res.status(400).send("user not created successfully " + error.message)
+            } catch (error) {
+        res.status(400).send( error.message +"!!!")
 
     }
 })
@@ -42,7 +65,8 @@ authRouter.post("/login", async (req, res) => {
             throw new Error("Password in Wronge!!!")
         }
         if (isPasswordValid) {
-            const token =  await user.getJWT();
+            const token =  await user.getJWT()
+        
             res.cookie("token",token,{
                 expires : new Date(Date.now() + 8 * 3600000)
             })
